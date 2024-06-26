7b11d61db356259dc7dc00263fb020f9d253eef3
samundra <samundra.devkota77@gmail.com>
2024-06-26 14:54:10 +0545

    test

---
+++ hello-world-api/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+ 
+namespace hello_world_api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+ 
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+ 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+ 
+            app.UseRouting();
+ 
+            app.UseAuthorization();
+ 
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
+
